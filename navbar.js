46ef478472422783d790a4901bf64181b14844c8
JSP <julianne.peters@gmail.com>
2018-12-29 09:28:37 -0800

    inital commit

---
+++ navbar.js
@@ -0,0 +1,30 @@
+function openSlideMenu() {
+    document.getElementById('side-menu').style.width = '250px';
+    document.getElementById('main').style.martinRight = '250px';
+}
+
+function closeSlideMenu() {
+    document.getElementById('side-menu').style.width = '0px';
+    document.getElementById('main').style.martinRight = '0px';
+}
+
+
+
+
+
+// function openNav() {
+//     document.getElementById("sideNav").style.width = "250px";
+// }
+//
+// function closeNav() {
+//     document.getElementById("sideNav").style.width = "0";
+// }
+
+// function minimizeNavLinks() {
+//     var x = document.getElementById("headerRight");
+//     if (x.className === "headerRight") {
+//         x.className += " responsive";
+//     } else {
+//         x.className = "headerRight";
+//     }
+// }
